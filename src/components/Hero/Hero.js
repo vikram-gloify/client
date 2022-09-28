7b8824cf5ad4a23efc5c394230c8849afde4cd07
vikramSingh <er.vikramsingh10@gmail.com>
2022-09-28 14:47:42 +0530

    footer add and home page define all components

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,14 @@
+import {Box,Container} from "@mui/material";
+
+const Hero = () => {
+  return (
+    <Box>
+        <Container>
+        
+
+        </Container>
+    </Box>
+  )
+}
+
+export default Hero
--- src/navigation/RoutingConfig.js
+++ src/navigation/RoutingConfig.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Footer from "../components/Footer/Footer"
 import Home from "../page/Home/Home"
 
 const RoutingConfig = () => {
@@ -7,7 +8,7 @@ const RoutingConfig = () => {
     <Routes>
         <Route  path="/" exact element={<Home/>}/>
     </Routes>
-      
+      <Footer/>
     </Router>
   )
 }
